c0d9b42a65d8fa93f2359549ca58535e44dd2019
Ahmad <ahmadmazloum98@gmail.com>
2022-03-25 18:10:02 +0200

    joining threads before the print

--- src/com/example/concurrency/Concurrency.java
+++ src/com/example/concurrency/Concurrency.java
@@ -1,6 +1,8 @@
 package com.example.concurrency;
 
-public class Concurrency extends  Thread{
+import java.util.concurrent.CountDownLatch;
+
+public class Concurrency{
     private static int balance = 0;
     static int TRANSACTIONS_PER_MACHINE  = 100000000;
     private static void deposit() {
@@ -9,8 +11,7 @@ public class Concurrency extends  Thread{
     private static void withdraw() {
         balance = balance - 1;
     }
-
-    public void run() {
+    public void machine() {
         synchronized(this) {
             for (int i = 0; i < TRANSACTIONS_PER_MACHINE; ++i) {
                 deposit(); // put a dollar in
@@ -18,13 +19,25 @@ public class Concurrency extends  Thread{
             }
         }
     }
-    public static void main(String[] args){
-        Concurrency c1 = new Concurrency();
-        Concurrency c2 = new Concurrency();
-        Concurrency c3 = new Concurrency();
-        c1.start();
-        c2.start();
-        c3.start();
+    public static void main(String[] args) throws InterruptedException {
+        Concurrency c = new Concurrency();
+        Thread firstThread = new Thread(() -> {
+           c.machine();
+        });
+        Thread secondThread = new Thread(() -> {
+            c.machine();
+        });
+        Thread thirdThread = new Thread(() -> {
+            c.machine();
+        });
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        firstThread.join();
+        secondThread.join();
+        thirdThread.join();
+
         System.out.println(balance);
     }
 }
